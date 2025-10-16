2b18467b9b2b6502563d4f045f1fb715f7c627f1
litvast <litvastplay2000@gmail.com>
2025-10-16 17:29:59 +0300

    Сделан весь функционал приложения по ТЗ

---
+++ src/main/java/org/ket/seconddemoprogram/Application.java
@@ -0,0 +1,24 @@
+package org.ket.seconddemoprogram;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Application extends javafx.application.Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("program-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+        stage.toFront();
+        stage.requestFocus();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
